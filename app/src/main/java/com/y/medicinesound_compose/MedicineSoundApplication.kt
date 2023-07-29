13d647651fd223dcd7c46d077face0e50b76b0a3
Y <hwiyul_g@daum.net>
2023-07-29 18:43:03 +0900

    Add : DI 준비

---
+++ app/src/main/java/com/y/medicinesound_compose/MedicineSoundApplication.kt
@@ -0,0 +1,14 @@
+package com.y.medicinesound_compose
+
+import android.app.Application
+import androidx.compose.material3.ExperimentalMaterial3Api
+
+@ExperimentalMaterial3Api
+class MedicineSoundApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+
+
+}
---
+++ app/src/main/java/com/y/medicinesound_compose/data/AppContainer.kt
@@ -0,0 +1,5 @@
+package com.y.medicinesound_compose.data
+
+interface AppContainer {
+    // val 어쩌구 Repository : 어쩌구Repository
+}
