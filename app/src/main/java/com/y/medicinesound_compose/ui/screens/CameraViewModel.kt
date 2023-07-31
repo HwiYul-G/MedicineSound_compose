b019e5f0380d7107ccdc0a4dd0e12553a4c287ca
Y <hwiyul_g@daum.net>
2023-07-31 13:54:10 +0900

    Add : Camera VM에서 useCase를 주입받아서 사용함.

--- app/src/main/java/com/y/medicinesound_compose/ui/screens/CameraViewModel.kt
+++ app/src/main/java/com/y/medicinesound_compose/ui/screens/CameraViewModel.kt
@@ -1,33 +1,55 @@
 package com.y.medicinesound_compose.ui.screens
 
 import android.content.Context
+import android.net.Uri
 import androidx.camera.view.PreviewView
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.y.medicinesound_compose.domain.repositories.CustomCameraRepo
+import com.y.medicinesound_compose.domain.usecases.CaptureAndSaveImageUseCase
+import com.y.medicinesound_compose.domain.usecases.GetCameraPreviewUseCase
+import com.y.medicinesound_compose.domain.usecases.GetUrlCameraUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+
+
 @HiltViewModel
 class CameraViewModel @Inject constructor(
-    private val customCameraRepo: CustomCameraRepo,
+    private val getUriCameraUseCase: GetUrlCameraUseCase,
+    private val showCameraPreviewUseCase: GetCameraPreviewUseCase,
+    private val captureAndSaveImageUseCase: CaptureAndSaveImageUseCase,
 ) : ViewModel() {
+
+    private val _imageUri = MutableStateFlow<Uri?>(null)
+    val imageUri : StateFlow<Uri?> = _imageUri
+
+
+    init{
+        viewModelScope.launch {
+            getUriCameraUseCase().collect{
+                _imageUri.value = it
+            }
+        }
+    }
 
     fun showCameraPreview(
         previewView: PreviewView,
         lifecycleOwner: LifecycleOwner,
     ) {
         viewModelScope.launch {
-            customCameraRepo.showCameraPreview(previewView, lifecycleOwner)
+            showCameraPreviewUseCase.showCameraPreview(previewView, lifecycleOwner)
         }
     }
 
     fun captureAndSave(context : Context){
         viewModelScope.launch {
-            customCameraRepo.captureAndSaveImage(context)
+            captureAndSaveImageUseCase.captureAndSaveImage(context)
         }
     }
+
 
 }
