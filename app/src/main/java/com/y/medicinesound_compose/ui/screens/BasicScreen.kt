55a25b6409bd8afd06bb9054d9762884a4b3c803
Y <hwiyul_g@daum.net>
2023-08-01 15:01:30 +0900

    ADD : 약 이름을 통해 해당 약의 effect를 받아오는 함수 작성 + basicscreen에 적용.

--- app/src/main/java/com/y/medicinesound_compose/ui/screens/BasicScreen.kt
+++ app/src/main/java/com/y/medicinesound_compose/ui/screens/BasicScreen.kt
@@ -22,6 +22,7 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -42,6 +43,7 @@ import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import com.y.medicinesound_compose.R
 import com.y.medicinesound_compose.utils.NavDestination
+import androidx.compose.runtime.getValue
 
 
 @OptIn(ExperimentalPermissionsApi::class, ExperimentalCoilApi::class)
@@ -52,6 +54,7 @@ fun BasicScreen(
     basicViewModel: BasicScreenViewModel = hiltViewModel()
 ) {
     // TODO : imageUri를 basicViewModel 내부에 사용자에게 보여주기 위한 상태로 담아야해!
+    val basicUiState by basicViewModel.uiState.collectAsState()
 
     // 앱 키자마자 모든 권한을 요청
     val permissions = if (Build.VERSION.SDK_INT <= 28) {
@@ -77,63 +80,89 @@ fun BasicScreen(
     }
 
 
-    if(imageUri == null) {
-        // UI
+//    if (imageUri == null) {
+    // UI
+    Column(
+        modifier = Modifier.verticalScroll(rememberScrollState()),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+
+        Image(
+            painter = if (imageUri == null) painterResource(R.drawable.medicine_temp_img)
+            else rememberImagePainter(data = imageUri),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(320.dp),
+            contentScale = ContentScale.Crop,
+        )
+
+        Spacer(modifier = Modifier.padding(vertical = 4.dp))
+
         Column(
-            modifier = Modifier,
-            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.padding(16.dp, 8.dp)
         ) {
-
-            Image(
-                painter = painterResource(R.drawable.medicine_temp_img),
-                contentDescription = null,
-                modifier = Modifier.fillMaxWidth(),
-                contentScale = ContentScale.Crop,
+            BasicButton(
+                btnTextResId = R.string.btn_camera,
+                btnIconResId = R.drawable.camera_black,
+                onClicked = { navController.navigate(NavDestination.CAMERA_SCREEN) }
             )
-
             Spacer(modifier = Modifier.padding(vertical = 4.dp))
+            BasicButton(
+                btnTextResId = R.string.btn_cognize,
+                btnIconResId = R.drawable.check_black,
+                onClicked = {
+                    if (basicUiState.medicineImg.isNotEmpty()) {
+                        // medicineImg에 있는 url을 이용해서 이미지를 분석 이때 tflite에 대한 처리 필요.
+                    } else {
+                        // TODO : 사진 촬영을 먼저하라는 안내가 필요함.
+                    }
+                }
+            )
+            Spacer(modifier = Modifier.padding(vertical = 4.dp))
+            BasicButton(
+                btnTextResId = R.string.btn_extract_info,
+                btnIconResId = R.drawable.extract_black,
+                onClicked = {
+                    if (basicUiState.medicineName.isNotEmpty()) {
+                        basicViewModel.getEYakEffect(basicUiState.medicineName)
+                    }
+                    // TODO : else일 때 사진을 찍고 인식하기 버튼을 사용하라는 안내가 필요
+                }
+            )
+        }
 
-            Column(
-                modifier = Modifier.padding(16.dp, 8.dp)
-            ) {
-                BasicButton(
-                    btnTextResId = R.string.btn_camera,
-                    btnIconResId = R.drawable.camera_black,
-                    onClicked = { navController.navigate(NavDestination.CAMERA_SCREEN) }
-                )
-                Spacer(modifier = Modifier.padding(vertical = 4.dp))
-                BasicButton(
-                    btnTextResId = R.string.btn_cognize,
-                    btnIconResId = R.drawable.check_black,
-                    onClicked = { /*TODO*/ }
-                )
-                Spacer(modifier = Modifier.padding(vertical = 4.dp))
-                BasicButton(
-                    btnTextResId = R.string.btn_extract_info,
-                    btnIconResId = R.drawable.extract_black,
-                    onClicked = { /*TODO*/ }
-                )
-            }
-
-            Column(
-                modifier = Modifier
-            ) {
-                TitleAndContentText(title = "제품명", content = "제품 이름")
-                Spacer(modifier = Modifier.padding(vertical = 4.dp))
+        Column(
+            modifier = Modifier
+        ) {
+            TitleAndContentText(title = "제품명", content = "제품 이름")
+            Spacer(modifier = Modifier.padding(vertical = 4.dp))
+            if (basicUiState.medicineEffect.isNotEmpty()) {
                 TitleAndContentText(
                     title = "효능 및 효과",
-                    content = "이 약은 해열 및 감기에 의한 동통(통증)과 두통, 치통, 근육통, 허리통증, 생리통, 관절통의 완화에 사용합니다."
+                    content = basicUiState.medicineEffect
+                )
+            } else {
+                TitleAndContentText(
+                    title = "효능 및 효과",
+                    content = "이미지가 없는 경우"
                 )
             }
         }
-    }else{
-        basicViewModel.updateUiStateImageUri(imageUri)
-        BasicScreenContentWithImage(imageUri = imageUri, navController = navController)
     }
+//    } else {
+//        basicViewModel.updateUiStateImageUri(imageUri)
+//        BasicScreenContentWithImage(imageUri = imageUri, navController = navController)
+//    }
 }
 
+@OptIn(ExperimentalCoilApi::class)
 @Composable
-fun BasicScreenContentWithImage(imageUri: String?, navController: NavController) {
+fun BasicScreenContentWithImage(
+    imageUri: String?,
+    navController: NavController,
+    basicViewModel: BasicScreenViewModel = hiltViewModel()
+) {
     // UI
     Column(
         modifier = Modifier.verticalScroll(rememberScrollState()),
@@ -143,7 +172,9 @@ fun BasicScreenContentWithImage(imageUri: String?, navController: NavController)
         Image(
             painter = rememberImagePainter(data = imageUri),
             contentDescription = "촬영된 사진",
-            modifier = Modifier.fillMaxWidth().height(300.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp),
             contentScale = ContentScale.Crop
         )
         Spacer(modifier = Modifier.padding(vertical = 4.dp))
@@ -166,7 +197,7 @@ fun BasicScreenContentWithImage(imageUri: String?, navController: NavController)
             BasicButton(
                 btnTextResId = R.string.btn_extract_info,
                 btnIconResId = R.drawable.extract_black,
-                onClicked = { /*TODO*/ }
+                onClicked = { basicViewModel.getEYakEffect("타이레놀") }
             )
         }
 
@@ -177,7 +208,7 @@ fun BasicScreenContentWithImage(imageUri: String?, navController: NavController)
             Spacer(modifier = Modifier.padding(vertical = 4.dp))
             TitleAndContentText(
                 title = "효능 및 효과",
-                content = "이 약은 해열 및 감기에 의한 동통(통증)과 두통, 치통, 근육통, 허리통증, 생리통, 관절통의 완화에 사용합니다."
+                content = "이미지 정보를 가진 스크린인 경우!"
             )
         }
     }
--- app/src/main/java/com/y/medicinesound_compose/ui/screens/BasicScreenViewModel.kt
+++ app/src/main/java/com/y/medicinesound_compose/ui/screens/BasicScreenViewModel.kt
@@ -1,31 +1,62 @@
 package com.y.medicinesound_compose.ui.screens
 
+
 import android.util.Log
 import androidx.lifecycle.ViewModel
-import com.y.medicinesound_compose.domain.repositories.CustomCameraRepo
+import androidx.lifecycle.viewModelScope
+import com.y.medicinesound_compose.domain.usecases.EYak.GetEYakListUseCase
 import com.y.medicinesound_compose.model.BasicUiState
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class BasicScreenViewModel @Inject constructor(
-//    private val eYakService: EYakService,
+    private val getEYakListUseCase: GetEYakListUseCase,
 ) : ViewModel() {
 
     private val _uiState = MutableStateFlow(BasicUiState())
     val uiState = _uiState.asStateFlow()
 
-    fun updateUiStateImageUri(imageUri : String){
-        _uiState.update { currentState->
+    fun updateUiStateImageUri(imageUri: String) {
+        _uiState.update { currentState ->
             currentState.copy(
                 medicineImg = imageUri
             )
         }
     }
 
+    fun getEYakEffect(itemName: String) {
+        viewModelScope.launch {
+            try {
+                // Log.d("Debug", "getEYakEffect called with itemName: $itemName")
+                val result = getEYakListUseCase(itemName)
+                if (result.isSuccess) {
+                    // Log.d("Debug", "getEYakEffect result: ${result.getOrNull()}")
+                    val list = result.getOrNull()
+                    list?.let {
+                        updateUiStateMedicineEffect(it[0].efcyQesitm)
+                    }
+                } else {
+                    // Log.d("Debug", "getEYakEffect failed: ${result.exceptionOrNull()}")
+                }
+            } catch (e: Exception) {
+                Log.e("Debug", "Exception occurred: ${e.message}")
+            }
+        }
+    }
+
+    private fun updateUiStateMedicineEffect(medicineEffect: String) {
+        Log.d("Debug", "updateUiStateMedicineEffect called with medicineEffect: ${medicineEffect}")
+        _uiState.update { currentState ->
+            currentState.copy(
+                medicineEffect = medicineEffect
+            )
+        }
+    }
 
 
 }
