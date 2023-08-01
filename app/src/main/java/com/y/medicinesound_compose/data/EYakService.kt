76027f8750a7b8cb446457d63719b8ee5156133e
Y <hwiyul_g@daum.net>
2023-08-01 14:59:21 +0900

    Add : EYakService.kt retrofit에게 어떻게 하라고 행동을 지시하는 부분.

---
+++ app/src/main/java/com/y/medicinesound_compose/data/EYakService.kt
@@ -0,0 +1,22 @@
+package com.y.medicinesound_compose.data
+
+import com.y.medicinesound_compose.data.remote.dto.EYakResponse
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+// DECODING KEY
+private const val SERVICE_KEY = "PL0dac515cDOUshijeLobFFiVPAjOIzC2YlxN0ewrilBop3oZOquTgmyZm2dwVUCv69vG3XBMdqnVkARzXkygA=="
+// BASE_URL = "http://apis.data.go.kr/1471000/DrbEasyDrugInfoService/getDrbEasyDrugList"
+// base_url?serviceKey={service_key} & type = json & numOfRows = 3 & itemName = {item_name}
+interface EYakService {
+
+    @GET("getDrbEasyDrugList")
+    suspend fun getEYakList(
+        @Query("serviceKey") serviceKey : String = SERVICE_KEY,
+        @Query("type") type : String = "json",
+        @Query("numOfRows") numOfRows : Int = 50,
+        @Query("itemName") itemName : String,
+    ) : Response<EYakResponse>
+
+}
